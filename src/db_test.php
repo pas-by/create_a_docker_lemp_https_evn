054bd8aabaf08b79d08269b5adadaac636251cac
tf choi <pas_by@yahoo.com>
2025-04-14 23:47:34 +0800

    modified the test ssl connection

--- src/db_test.php
+++ src/db_test.php
@@ -9,9 +9,9 @@ PDO::MYSQL_ATTR_SSL_CA => './nginx.crt',
 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false);
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password, $opts);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $stmt = $conn->query("show session status");
+  $stmt = $conn->query("show session status like 'Ssl%'");
 
   foreach($stmt as $row){
     echo $row["Variable_name"] . ',' . $row["Value"] . '<br />';
